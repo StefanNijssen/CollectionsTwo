8234ea91f31c443611eb4ae9b7a7358f5cd84652
StefanNijssen <stefan.nijssen68@gmail.com>
2022-01-11 20:26:56 +0100

    goed begin bovenste en onderste deel

--- yatzee.py
+++ yatzee.py
@@ -4,10 +4,11 @@ from collections import Counter
 def throwDice(numberOfDice):
     list = []
     min = 1
-    max = 6
+    max = 5
     for i in range(numberOfDice):
         throw = random.randint(min, max)
         list.append(throw)
+    print(list)
     return list
     
 def countNumbers(list):
@@ -35,12 +36,35 @@ def reroll(list):
     else:
         print("U heeft deze nummers gerollt.")
 
-random = (throwDice(6))
-print(random)
-amountOfNumbers = int(input("Kies hoeveel cijfers u wilt noteren 1-6\n"))
-print("U mag " + str(amountOfNumbers) + " cijfers kiezen.")
+#random = (throwDice(6))
+#print(random)
+#amountOfNumbers = int(input("Kies hoeveel cijfers u wilt noteren 1-6\n"))
+#print("U mag " + str(amountOfNumbers) + " cijfers kiezen.")
 
-countNumbers(random)
+#countNumbers(random)
+
+Dice = throwDice(5)
+
+upper_Half = ['ones', 'twos', 'threes', 'fours', 'fives', 'sixes']
+
+bottom_Half = ['TOAK', 'FOAK', 'Full_House', 'SmSt', 'LaSt', 'Yatzhee', 'Chance']
+
+half_Choice = input('Wilt u de score in het bovenste of onderste gedeelte invullen?')
+
+if half_Choice == 'bovenste':
+    which_Number = int(input('Welk nummer wil je invullen?'))
+    amount_Number = Dice.count(which_Number)
+    total_Number = amount_Number *(which_Number)
+    print(total_Number)
+elif half_Choice == 'onderste': 
+    which_Combination = input('Welke combinatie wilt u invullen')
+    if which_Combination == 'TAOK':
+        which_Number = int(input('Welk nummer wil je invullen?'))
+        amount_Number = Dice.count(which_Number)
+        total_Number = amount_Number *(which_Number)
+        if amount_Number >= 3:
+            bottom_Half[0] = total_Number
+print(bottom_Half)
 
 
 
@@ -59,24 +83,18 @@ countNumbers(random)
 
 
 
-
-
-
-
-
-
-def combinations(list):
-    threeOfaKind = 3
-    if 3 in list == 3:
+# def combinations(list):
+#     threeOfaKind = 3
+#     if 3 in list == 3:
         
-        print('3')
-    #if Counter == 3:
-        #print('3')
-    else:
-         print('2')
+#         print('3')
+#     if Counter == 3:
+#         #print('3')
+#     else:
+#          print('2')
 
-    #if len(list) == 3:
-       #print("3")
+#     if len(list) == 3:
+#        print("3")
 
     
-#print(combinations(throwDice(3)))
+# print(combinations(throwDice(3)))
